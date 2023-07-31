dd2fb55638bf7e16ca557f8a8f632e5b9b04c074
pengkangjie <pengkangjie@insta360.com>
2023-07-31 19:47:22 +0800

    初始化整个项目

---
+++ app/src/main/java/com/example/learncoroutine/CountViewModel.kt
@@ -0,0 +1,63 @@
+package com.example.learncoroutine
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.*
+import java.text.DecimalFormat
+
+class CountViewModel: ViewModel() {
+
+    private val _countdownTime = MutableLiveData<Double>()
+
+    val countdownTime: LiveData<Double>
+        get() = _countdownTime
+
+    private val _isCountingDown = MutableLiveData<Boolean>()
+
+    val isCountingDown: LiveData<Boolean>
+        get() = _isCountingDown
+
+    private var job: Job? = null
+
+    private var remainingTime = 0.0
+    private val decimalFormat = DecimalFormat("0.00")
+
+    fun formatTime(timeInSeconds: Double): String {
+        return decimalFormat.format(timeInSeconds)
+    }
+
+    fun startCountdown(timeInSeconds: Double) {
+        if (job?.isActive == true) {
+            job?.cancel() // 如果有正在运行的倒计时，先取消它
+        }
+
+        job = viewModelScope.launch {
+            remainingTime = formatTime(timeInSeconds).toDouble()
+            while (remainingTime > 0) {
+                delay(10) // 等待10毫秒
+                remainingTime -= 0.01 // 每次减去0.01秒
+                remainingTime = formatTime(remainingTime).toDouble()
+                _countdownTime.value = remainingTime
+            }
+        }
+        _isCountingDown.value = true
+    }
+
+    fun pauseCountdown() {
+        job?.cancel()
+        _isCountingDown.value = false
+    }
+
+    fun resumeCountdown() {
+        startCountdown(remainingTime)
+        _isCountingDown.value = true
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        pauseCountdown()
+    }
+
+}
---
+++ app/src/main/java/com/example/learncoroutine/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.learncoroutine
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.viewModels
+import com.example.learncoroutine.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val countViewModel: CountViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnStop.text = "暂停"
+        initListener()
+    }
+
+    private fun initListener() {
+        countViewModel.isCountingDown.observe(this) {
+            binding.btnStop.text = if (it) "暂停" else "恢复"
+        }
+
+        countViewModel.countdownTime.observe(this) {
+            binding.tvCount.text = countViewModel.formatTime(it)
+        }
+
+        binding.btnStart.setOnClickListener {
+            countViewModel.startCountdown(binding.etInput.text.toString().toDoubleOrNull() ?: 0.0)
+        }
+
+        binding.btnStop.setOnClickListener {
+            if (countViewModel.isCountingDown.value == true) {
+                countViewModel.pauseCountdown()
+            } else {
+                countViewModel.resumeCountdown()
+            }
+        }
+    }
+}
